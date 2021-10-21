fd8379455dd32c47d0891d50f72bd35f08dc1305
Tathagata Talukdar <tathagata.talukdar@gmail.com>
2021-10-21 14:47:08 +0530

    post comments added

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('',(req,res) => {
 
 app.route("/getcomments")
 .post(function(req,res){
-    let sql2 = `SELECT * FROM comments WHERE commentor='${req.body.sessionUser}' OR self_common='common'`;
+    let sql2 = `SELECT * FROM comments WHERE (commentor='${req.body.sessionUser}' OR self_common='common') AND resolved = 0`;
     let query = db.query(sql2, (err, results) => {
         if(err) throw err;
         res.send(results);
@@ -35,16 +35,38 @@ app.route("/getcomments")
 
 app.route("/rescomments")
 .post(function(req,res){
-    console.log(req.body.id);
-    let sql = `UPDATE comments SET resolved = 0 WHERE id = ${req.body.id}`
+    let sql = `UPDATE comments SET resolved = 1 WHERE id = ${req.body.id}`
+    let query = db.query(sql,(err,results) => {
+        if(err) throw err;
+        else console.log('db updated!');
+    });
     res.send('put method called');
 });
 
 app.route("/postcomments")
-.post(function(req,res){
-   console.log(req);
+.post(function(req,res){ 
+    var id;  
+    let sql = `SELECT COUNT (id) AS count FROM comments `; 
+    let query = db.query(sql,(err,results) => {
+        if(err) throw err;
+        else {
+            id = results[0].count+1;
+            let sql2 = `INSERT INTO comments VALUES (${id},'${req.body.commentor}','${req.body.self_common}','${req.body.comment_}','${req.body.ToC}',${0});`;
+            let query2 = db.query(sql2,(err,results)=>{
+                if(err) throw err;
+                else {
+                    let sql3 = `SELECT * FROM comments WHERE (commentor='${req.body.commentor}' OR self_common='common') AND resolved = 0`;
+                    let query3 = db.query(sql3,(err,results)=>{
+                        if(err) throw err;
+                        else{
+                            res.send(results);
+                        }
+                    })
+                };
+            });
+        }
+    });
 });
-
 
 
 app.listen(PORT);
